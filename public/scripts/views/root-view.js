4ddd75c4555492fe465b5bb61880a93ce8427ff4
tfoucault <thibault.foucault@gmail.com>
2015-11-30 00:49:51 +0100

    Modal management

--- public/scripts/views/root-view.js
+++ public/scripts/views/root-view.js
@@ -12,7 +12,20 @@ define(['marionette','templates','backbone.radio'], function(Marionette, JST, Ra
 
     // Add regions to our main layout
     regions: {
-      mainRegion: "#main-content"
+      mainRegion: "#main-content",
+      modalRegion: "#modal-content"
+    },
+
+    initialize: function() {
+      // Get the channel whom name is 'main'
+      var mainChannel = Radio.channel('main');
+      // Listen to views that want to display a modal content
+      this.listenTo(mainChannel, 'modal:show', this.showModal);
+    },
+
+    showModal: function(params) {
+      var modal = params.modal;
+      this.modalRegion.show(modal);
     }
   });
 
